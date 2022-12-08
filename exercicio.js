c1e24b15382c97c32f71c34021190c463a431763
Tales de Melo <melotales@hotmail.com>
2022-12-08 00:11:18 -0300

    correcao exercicios js

--- exercicio.js
+++ exercicio.js
@@ -1,15 +1,28 @@
-function livro (nome, ano, autor) {
-  const nomeMaior = nome.toUpperCase;
-  ano = 2050 - ano;
-  frase = nome + " por " + autor;
-  objeto = {
-    nome,
-    ano,
-    autor
-  }
-  return objeto;
+
+
+// function livro(nome, ano, autor) {
+//   const nomeMaior = nome.toUpperCase();
+//   const totalAnos = 2050 - ano;
+//   const frase = nome + " por " + autor;
+//   const objeto = {
+//     nome: nomeMaior,
+//     totalAnos,
+//     frase,
+//   };
+//   return objeto;
+// }
+
+
+//versão reduzida da function:
+
+function livro(nome, ano, autor) {
+  return {
+    nome: nome.toUpperCase(),
+    totalAnos: 2050 - ano,
+    frase: nome + " por " + autor
+  };
 }
 
-livro ("O Senhor dos Anéis", 1954, "J. R. R. Tolkien")
+const variavel = livro("O Senhor dos Anéis", 1954, "J. R. R. Tolkien");
 
-const variavel = console.log(livro);
+console.log(variavel);
